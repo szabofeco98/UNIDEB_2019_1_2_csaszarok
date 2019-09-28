81c86917b527cf1d213ecb587e643be847885a0b
Wrigozo <szaboalexandra18@gmail.com>
2019-09-28 14:06:33 +0200

    added loginview and logincontroller

--- App/src/main/java/software/controller/LoginController.java
+++ App/src/main/java/software/controller/LoginController.java
@@ -4,7 +4,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import software.app.Main;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import org.hibernate.annotations.common.util.impl.LoggerFactory;
 
 
 import java.net.URL;
@@ -13,9 +15,30 @@ import java.util.ResourceBundle;
 public class LoginController implements Initializable {
 
 
+    @FXML
+    private AnchorPane login;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private Button sigin;
+
+    @FXML
+    private Button signup;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+    @FXML
+    private void initialize() {
+        // TODO
+        login.setVisible(true);
+        //logger.info("The game has been initialized successfully!");
     }
 
     public void test(ActionEvent actionEvent) {
